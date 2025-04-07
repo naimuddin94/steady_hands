54dd081440c91ecc4da636a7638e37b0af947f4a
naimuddin94 <developernaimuddin@gmail.com>
2025-04-07 14:56:13 +0600

    Removes unused artist type constants

--- src/app/modules/Auth/auth.constant.ts
+++ src/app/modules/Auth/auth.constant.ts
@@ -8,10 +8,4 @@ export const ROLE = {
 
 export type TRole = keyof typeof ROLE;
 
-export const ARTIST_TYPE = {
-  TATOO_ARTIST: 'Tattoo Artist',
-  PIEREER: 'Piereer',
-} as const;
-
 export type ValueOf<T> = T[keyof T];
-export type TArtistType = ValueOf<typeof ARTIST_TYPE>;
