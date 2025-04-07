7f81b2a2efa1308097794ecbd98da7a0206af438
naimuddin94 <developernaimuddin@gmail.com>
2025-04-07 14:55:59 +0600

    Defines artist interface

--- src/app/modules/Artist/artist.interface.ts
+++ src/app/modules/Artist/artist.interface.ts
@@ -1,6 +1,23 @@
 import { Document } from 'mongoose';
-import { TArtistType } from '../Auth/auth.constant';
+import {
+  ExpertiseType,
+  TArtistType,
+  TContact,
+  TServices,
+} from './artist.contant';
+import { locationSchema } from '../Location/location.model';
 
 export interface IArtist extends Document {
   type: TArtistType;
+  expertise: ExpertiseType[];
+  image?: string;
+  location: typeof locationSchema;
+  city: string;
+  idCardFront: string;
+  idCardBack: string;
+  selfieWithId: string;
+  profileViews: number;
+  services: TServices;
+  contact: TContact;
+  description: string;
 }
